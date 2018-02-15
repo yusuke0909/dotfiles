d255362292b4f9e3c5733bdb27a20cabffd6cf80
yusuke0909 <yusuke.liveforever@gmail.com>
2018-02-15 13:38:08 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+DIR=$(pwd)
 DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .gitignore .vimrc .tmux.conf .sshrc .sshrc.d .dircolors bin )
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $file $HOME/$file
+    ln -s ${DIR}/$file $HOME/${file}
 done
