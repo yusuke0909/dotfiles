5daee8141fa6942122a2e26da70d023aca1a79c6
yusuke0909 <yusuke.liveforever@gmail.com>
2018-02-15 13:27:41 +0900

    fix ghq settings

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .giti
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $file $HOME/$file
 done
