f0fd9043e4610911006755a59f12df644ee16a8f
yusuke0909 <yusuke.liveforever@gmail.com>
2018-02-15 15:26:03 +0900

    add peco settings

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$(pwd)
-DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .gitignore .vimrc .tmux.conf .sshrc .sshrc.d .dircolors bin )
+DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .gitignore .vimrc .tmux.conf .peco .sshrc .sshrc.d .dircolors bin )
 
 for file in ${DOT_FILES[@]}
 do
