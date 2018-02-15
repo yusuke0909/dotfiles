51065219485e89b937979ee1926d8dcf7fd26bc3
yusuke0909 <yusuke.liveforever@gmail.com>
2018-02-15 14:26:49 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .giti
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s ${DIR}/$file $HOME/${file}
+    ln -s ${DIR}/${file} $HOME/${file}
 done
